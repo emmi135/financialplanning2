dd3f7aaa1bc86d09789d82a4e51478373e1b06fc
adnbal <99461328+adnbal@users.noreply.github.com>
2025-07-08 12:36:28 +1200

    Update budget_invest_app.py

--- budget_invest_app.py
+++ budget_invest_app.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 # Load secrets
 genai.configure(api_key=st.secrets["gemini"]["api_key"])
 OPENROUTER_API_KEY = st.secrets["openrouter"]["api_key"]
-CHAT_API_ID = st.secrets["CHAT_API_ID"]
+
 CHAT_API_ID = st.secrets["botpress"]["chat_api_id"]
 BOTPRESS_TOKEN = st.secrets["botpress"]["token"]
 
